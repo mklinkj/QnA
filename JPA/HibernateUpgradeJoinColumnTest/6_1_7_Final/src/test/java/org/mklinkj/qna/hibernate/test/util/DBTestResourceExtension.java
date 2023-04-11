563b4e66aba0c82d4df3fc63536d9f3e46edd4e6
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-04-11 21:01:02 +0900

    Gradle 빌드 스크립트 정리
    - 빌드 스크립트의 중복내용을 통합하고 싶어서 정리했다.
    - 다른 스터디에서 진행했던 주석을 그대로 가져왔는데, 관련없어서 제거했다.

--- JPA/HibernateUpgradeJoinColumnTest/6_1_7_Final/src/test/java/org/mklinkj/qna/hibernate/test/util/DBTestResourceExtension.java
+++ JPA/HibernateUpgradeJoinColumnTest/6_1_7_Final/src/test/java/org/mklinkj/qna/hibernate/test/util/DBTestResourceExtension.java
@@ -7,14 +7,6 @@ import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.mklinkj.qna.hibernate.common.jpa.EMF;
 
-/**
- * 저자님이 JUnit 4 @Rule을 사용하여 구현한 내용을 JUnit 5의 @ExtendWith로 가능하게 바꿔보자!<br>
- * 코드 내용상 테스트 메서드 하나마다 EMF를 초기화하고 테스트 코드 본문 실행 후 닫는다.<br>
- * <br>
- * 아래 SpringExtension 클래스를 약간 참조했다.<br>
- * <a
- * href="https://github.com/spring-projects/spring-framework/blob/main/spring-test/src/main/java/org/springframework/test/context/junit/jupiter/SpringExtension.java">...</a>
- */
 public class DBTestResourceExtension
     implements BeforeEachCallback, AfterEachCallback, BeforeAllCallback, AfterAllCallback {
 

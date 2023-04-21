8632ae219e9ce99b95ddc78dbf19ca0b3249239f
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-04-22 00:09:38 +0900

    Prettier 설정 추가

--- React/react-router-dom-tutorial/my-tutorial-app/src/main.jsx
+++ React/react-router-dom-tutorial/my-tutorial-app/src/main.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import Root from "./routes/root.jsx";
-import ErrorPage from "./error-page.jsx";
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import './index.css'
+import Root from './routes/root.jsx'
+import ErrorPage from './error-page.jsx'
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />
 
-    },
-]);
+  }
+])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)

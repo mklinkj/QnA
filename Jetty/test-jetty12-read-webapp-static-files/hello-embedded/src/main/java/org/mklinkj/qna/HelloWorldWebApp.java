059d6ea89259893431222b1b3a6c436ea0ebd048
mklinkj <96992046+mklinkj@users.noreply.github.com>
2023-12-19 06:58:04 +0900

    (실패)TLD 스캔에 javax.servlet.jsp.jstl-*만 읽을 수 있게 설정
    설정이 맞는지는 모르겠는데.. 😅
    동작이나, 로그 상으로는 괜찮은데...
    JSTL 라이브러리에서만 TLD를 읽도록 설정했다.
    ---
    이게 잘 되는 줄 알았는데..
    IntelliJ에서 바로 main 클래스를 실행하면 잘되는데,
    
    mvn clean compile exec:java 로 하면 또 tld 스캔이 안됨.ㅠㅠ

--- Jetty/test-jetty12-read-webapp-static-files/hello-embedded/src/main/java/org/mklinkj/qna/HelloWorldWebApp.java
+++ Jetty/test-jetty12-read-webapp-static-files/hello-embedded/src/main/java/org/mklinkj/qna/HelloWorldWebApp.java
@@ -1,5 +1,8 @@
 package org.mklinkj.qna;
 
+import org.apache.tomcat.util.scan.StandardJarScanFilter;
+import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.eclipse.jetty.ee8.apache.jsp.JettyJasperInitializer;
 import org.eclipse.jetty.ee8.webapp.WebAppContext;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.SymlinkAllowedResourceAliasChecker;
@@ -7,12 +10,22 @@ import org.eclipse.jetty.server.SymlinkAllowedResourceAliasChecker;
 public class HelloWorldWebApp {
 
   public static void main(String[] args) throws Exception {
-    Server server = new Server(8080);
+    var server = new Server(8080);
 
-    WebAppContext context = new WebAppContext();
+    var context = new WebAppContext();
     context.setContextPath("/");
     context.setBaseResourceAsString("src/main/webapp");
     context.addAliasCheck(new SymlinkAllowedResourceAliasChecker(context.getCoreContextHandler()));
+
+    var jarScanner = new StandardJarScanner();
+    var jarScanFilter = new StandardJarScanFilter();
+    jarScanFilter.setTldScan("javax.servlet.jsp.jstl-*");
+    jarScanFilter.setDefaultTldScan(false);
+
+    jarScanner.setJarScanFilter(jarScanFilter);
+    context.setAttribute("org.apache.tomcat.JarScanner", jarScanner);
+    new JettyJasperInitializer().onStartup(null, context.getServletContext());
+
     server.setHandler(context);
     server.start();
     server.join();
